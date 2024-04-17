05b994ea46ea77c620012d3f700b100727de40e0
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-04-17 12:39:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     <div className="App">
       <h1>Salary calculator</h1>
        <SalaryCalculator/>
+        <p>"How generous of me to calculate your salary down to the last penny."</p>
+       <h4>You're welcome, MUKUL KAUSHAL.</h4>
     </div>
   );
 }
