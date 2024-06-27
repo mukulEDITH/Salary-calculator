31a5c6e463fe731b109007ea4ad9b15473816412
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-06-27 22:47:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Salary calculator</h1>
+     <h6> This will provide a general estimation of your salary, though it may not be precise.</h6>
        <SalaryCalculator/>
         <p>"How generous of me to calculate your salary down to the last penny."</p>
        <h4>You're welcome...</h4>
